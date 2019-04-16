ab5ebb2425e3ffe8d7decdbccdc88917f821a4c3
YunusovSamat <44583676+YunusovSamat@users.noreply.github.com>
2019-04-16 17:35:51 +0300

    db

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Catalog
+
+
+class IndexView(generic.ListView):
+    template_name = 'main/index.html'
+    context_object_name = 'catalogs'
+    model = Catalog
+
